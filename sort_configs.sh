e9390978a498020aac9f20c9c03ac72ee3a1548c
Imti Majeed <imtiazmaj@gmail.com>
2022-05-25 13:51:10 -0700

    feat: add lint git workflow, throw error when in CI and config not sorted

--- sort_configs.sh
+++ sort_configs.sh
@@ -11,6 +11,13 @@ do
     # files are the same, so remove the tmp file
     /bin/rm "$f.tmp.json"
   else 
+    # if we're in CI (set to true for github actions)
+    # and the files are different, then fail
+    if [ "$CI" == true ]; then
+      echo 'Configs not sorted, failing...'
+      exit 125
+    fi
+
     # files are different, update the config with the tmp file
     /bin/mv "$f.tmp.json" "$f.json"
   fi
